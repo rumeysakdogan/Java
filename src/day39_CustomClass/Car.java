8b4aaf5a1e1ed283d89988b4964e40cb03e481cb
rumeysakdogan <dgn.rumeysaa@gmail.com>
2020-08-09 10:45:29 -0600

    update Car.java

--- src/day39_CustomClass/Car.java
+++ src/day39_CustomClass/Car.java
@@ -5,14 +5,33 @@ public class Car {
         Attributes:
             model, year, brand, color, mileage, price...
      */
-// instance variables are defined outside of methods.
+// instance variables are defined outside of methods.We give attributes inside of instance variables
 
     String brand;
-    int year;
     String model;
+    int year;
     String color;
     double mileage;
     double price;
+
+// instance methods do not have STATIC specifier.Instance methods are specific to instance objects.
+// We give actions/functions inside of instance methods.
+
+    public void setInfo(String carBrand, String carModel, int carYear,String carColor, double carMileage,double carPrice ){
+        brand = carBrand;
+        model = carModel;
+        year = carYear;
+        color = carColor;
+        mileage = carMileage;
+        price = carPrice;
+    }
+    // sets the info of car object
+
+    //2012 Toyota Corolla, Red, 10000, $45000
+    public void getInfo(){
+        System.out.println(year+" "+brand+" "+model+", "+color+", "+mileage+", $"+price);
+    }
+    // prints the carInfo
 
 
 }
--- src/day39_CustomClass/CarObjects.java
+++ src/day39_CustomClass/CarObjects.java
@@ -5,35 +5,41 @@ public class CarObjects {
 
 //   className objectName = keyword Constructor
         Car         car1  =   new      Car();
-
+/*
             car1.brand = "Toyota";
             car1.model = "Corolla";
             car1.year = 2020;
             car1.color = "White";
             car1.mileage = 20000;
             car1.price = 19000.50;
-
+*/
+        car1.setInfo("Toyota","Corolla",2020,"White",20000,19000.50);
+        car1.getInfo();
+/*
         System.out.println(car1.brand);
         System.out.println(car1.model);
         System.out.println(car1.year);
         System.out.println(car1.color);
-
+*/
         System.out.println("================================");
 
         Car car2 = new Car();
 
+        car2.setInfo("BMW","G 550",2019,"Red",2700,35000);
+        car2.getInfo();
+/*
             car2.brand = "BMW";
             car2.model = "G 550";
             car2.year = 2019;
             car2.color = "Red";
+*/
 
-        System.out.println(car2.brand);
-        System.out.println(car2.model);
-        System.out.println(car2.year);
-        System.out.println(car2.color);
+        System.out.println("================================");
 
+        Car car3 = new Car();
 
-
+        car3.setInfo("Honda","Odessy",2014,"Blue",90000,3000);
+        car3.getInfo();
 
 
     }
