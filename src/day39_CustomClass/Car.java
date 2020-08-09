c99e806302a8c534bed779957bde9aaf555a77b8
rumeysakdogan <dgn.rumeysaa@gmail.com>
2020-08-09 09:01:00 -0600

    "add CarObjects.java"

---
+++ src/day39_CustomClass/Car.java
@@ -0,0 +1,18 @@
+package day39_CustomClass;
+
+public class Car {
+    /*
+        Attributes:
+            model, year, brand, color, mileage, price...
+     */
+// instance variables are defined outside of methods.
+
+    String brand;
+    int year;
+    String model;
+    String color;
+    double mileage;
+    double price;
+
+
+}
