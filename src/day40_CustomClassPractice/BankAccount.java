1eff236759d823d146f9139c33c77feb43763390
rumeysakdogan <dgn.rumeysaa@gmail.com>
2020-08-12 17:42:44 -0600

    add BankAccount.java

---
+++ src/day40_CustomClassPractice/BankAccount.java
@@ -0,0 +1,52 @@
+package day40_CustomClassPractice;
+/*
+ create a custom class for BankAccount
+        attributes/data that can have are:
+                1. AccountHolder, 2. AccountNumber, 3. Balance, 4. AccountType
+          Actions: showBalance, deposit, withdraw
+          requirements:
+            1. user should be able to deposit money into their account
+            2. user should be able to withdraw money from their account
+                2.1 if the withdrawing account is greater than available balance, 35$ penalty charge will be added
+                2.2 if the balance is less than or equal to 0, user should not be able to withdraw money
+            3. user should be able to see their balance
+ */
+public class BankAccount {
+    String accountType;
+    String accountHolder;
+    String accountNumber;
+    double balance;
+
+    public void setAccountInfo(String userAccountType, String name, String userAccountNumber, double userBalance ){
+        accountType = userAccountType;
+        accountHolder = name;
+        accountNumber = userAccountNumber;
+        balance = userBalance;
+    }
+
+    public void getAccountInfo(){
+        System.out.println("===============================================");
+        System.out.println("Account Type: " + accountType);
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Available Balance" + balance);
+        System.out.println("Account Type: " + accountType);
+        System.out.println("===============================================");
+    }
+
+    public void checkBalance(){
+        System.out.println("Available Balance: " + balance);
+    }
+
+    public void deposit(double amount){
+        balance += amount;
+    }
+
+    public void withdraw(double amount){
+        balance -= amount;
+//        if (amount > balance ){
+//            balance -= 35;
+//        }
+    }
+
+}
---
+++ src/day40_CustomClassPractice/Offer.java
@@ -0,0 +1,17 @@
+package day40_CustomClassPractice;
+/*
+Offer:
+    Attributes:
+        Location, hasBenifit(boolean), salary, WFH(boolean), JobTitle
+    Actions:
+        SetInfo, getInfo
+    create a class named MyOffers
+        1. create 7 objects Offer Class and store them into an arraylist
+        2. write a program that can only keep the offers from local area
+        3. write program that can remove all the offers that are not SDET or QA
+        4. write a program that can remove all the offers that are not WFH
+        5. write a program that can remove all the offers that do not have benifits
+
+ */
+public class Offer {
+}
