5f795c79ddf5912222eea7caf623efd004ecd73f
rumeysakdogan <dgn.rumeysaa@gmail.com>
2020-08-15 08:57:39 -0600

    add Carpet.java

---
+++ src/day41_Static/Carpet.java
@@ -0,0 +1,52 @@
+package day41_Static;
+/*
+ 1. create a custom class for the carpet class that should contain the following:
+
+                instance variables:
+                        width, length, unitPrice, isPersian (boolean)
+                instance methods:
+                        customOrder(): sets the carpet' width, length, unitprice, & isPersian
+                        calcCost(): should be able to calculate the total cost of the carpet and return it as double
+                        getCarpetInfo: should be able to display all the info of the carpet including the total cost
+                        of the carpet as calculated by calcCost()
+            total price of carpet= (width*length)*unitprice
+            if the carpet is persian  carpet that's came from Turkey, add 200$ to the totalPrice
+            create a class called carpetObjects, and create an array of the carpet that contains 5 carpet objects
+            ( make sure you set the instance variables of those objets)
+            create two ArrayList of carpets:
+                                        persianCarpets
+                                        reugularCarpets
+            write a program to store all the persian Carpets into the list of persianCarpets, and store all
+             regular carpets into the list of regularCarpets
+                    use for each loop to print out all the persian carpets
+
+ */
+public class Carpet {
+
+    double width;
+    double length;
+    double unitPrice;
+    boolean isPersian;
+
+    public void customOrder(double width, double length, double unitPrice, boolean isPersian){
+        this.width = width;
+        this.length = length;
+        this.unitPrice = unitPrice;
+        this.isPersian = isPersian;
+    }
+
+    public double calcCost(){
+        double totalPrice = (width*length)*unitPrice;;
+        return (isPersian) ? (totalPrice + 200) : totalPrice;
+    }
+
+    public void getCarpetInfo(){
+        System.out.println("=========================================");
+        System.out.println("Width: " + width); // you need to use this.width if there is a var sharing same name as width
+        System.out.println("Length: " + length);
+        System.out.println("Unit Price: " + unitPrice);
+        System.out.println("Total Price: " + calcCost());
+        System.out.println("=========================================");
+    }
+
+}
