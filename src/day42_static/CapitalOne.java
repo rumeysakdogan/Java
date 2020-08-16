e727ff05c21a5715f1a39dd8ccab51ef606566eb
rumeysakdogan <dgn.rumeysaa@gmail.com>
2020-08-16 10:12:49 -0600

    update ScrumTeam.java

--- src/day42_static/CapitalOne.java
+++ src/day42_static/CapitalOne.java
@@ -31,5 +31,37 @@ public class CapitalOne {
         team1.addDevelopers(developers);
 
         System.out.println( team1 );
+
+        System.out.println("=====================================================");
+
+        for( Testers each : team1.testers){
+            System.out.println(each.name+" : $"+each.salary);
+        }
+        System.out.println("=====================================================");
+
+        for( Developers each : team1.developers){
+            System.out.println(each.name+" : $"+each.salary);
+        }
+        System.out.println("=====================================================");
+
+        team1.removeTester(1891);
+        team1.removeDeveloper(2007);
+        team1.removeDeveloper(2035);
+        System.out.println( team1 );
+
+        System.out.println("=====================================================");
+
+        Developers dev1 = new Developers();
+        dev1.setInfo("Burak",'M',6543L,"Frontend Dev",153000);
+        team1.addDeveloper(dev1);
+        System.out.println( team1 );
+
+        System.out.println("=====================================================");
+
+        ScrumTeam[] scrumTeams = { team1, new ScrumTeam(), new ScrumTeam() };
+        /*
+        print al the devs and testers from scrum team
+        find maximum salary of devs, maximum salary of testers
+         */
     }
 }
