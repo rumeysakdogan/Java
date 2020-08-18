78c029d806421e564f504fcccec0a3d7609818e2
rumeysakdogan <dgn.rumeysaa@gmail.com>
2020-08-18 17:58:53 -0600

    add Static_vs_Instance

--- src/day43_Static/Static_vs_Instance.java
+++ src/day43_Static/Static_vs_Instance.java
@@ -2,4 +2,28 @@ package day43_Static;
 
 public class Static_vs_Instance {
 
+    static int staticVariable;
+    int instanceVariable;
+
+    public static void main(String[] args) {
+
+        Static_vs_Instance obj1 = new Static_vs_Instance();
+        obj1.instanceVariable = 100;
+        obj1.staticVariable = 500;
+
+        Static_vs_Instance obj2 = new Static_vs_Instance();
+        obj2.instanceVariable = 200;
+        obj1.staticVariable = 400;
+
+        System.out.println( obj1.instanceVariable );
+        System.out.println( obj2.instanceVariable );
+
+        System.out.println("=======================================");
+
+        System.out.println( obj1.staticVariable);  // NOT preferred way to call static variables
+        System.out.println( obj2.staticVariable);
+
+        System.out.println( Static_vs_Instance.staticVariable ); // preferred way calling statics through classname
+    }
+
 }
