54bb2ce0e2bf55bcfce8aa0868d47a6b1b76010a
rumeysakdogan <dgn.rumeysaa@gmail.com>
2020-08-18 20:01:43 -0600

    update Data.java

--- src/day43_Static/Test.java
+++ src/day43_Static/Test.java
@@ -2,6 +2,11 @@ package day43_Static;
 
 import java.util.Arrays;
 
+import static library.Resources.a;  // imports only specific static members from Resources
+import static library.Resources.method1;
+
+import static library.Resources.*;  // imports all class members from Resources
+
 public class Test {
 
     public static void main(String[] args) {
@@ -16,5 +21,8 @@ public class Test {
 
         System.out.println(Arrays.toString(Data.arr));
         System.out.println(Data.list);
+
+        System.out.println(a);
+
     }
 }
