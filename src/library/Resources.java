ac2a6b6747722160cd359866cbf8490d150e940b
rumeysakdogan <dgn.rumeysaa@gmail.com>
2020-08-18 20:02:22 -0600

    add Resources.java

---
+++ src/library/Resources.java
@@ -0,0 +1,16 @@
+package library;
+
+public class Resources {
+
+    public static int a = 300;
+    int b = 200;
+
+    public static void method1(){
+        System.out.println("Static method");
+    }
+
+    public void method2(){
+        System.out.println("Instance Method");
+    }
+
+}
