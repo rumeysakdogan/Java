4d183f0d1ecf28a7f876f5e86f6c12c65f4a5895
rumeysakdogan <dgn.rumeysaa@gmail.com>
2020-08-18 19:39:47 -0600

    add Data.java

--- src/day43_Static/Test.java
+++ src/day43_Static/Test.java
@@ -1,5 +1,7 @@
 package day43_Static;
 
+import java.util.Arrays;
+
 public class Test {
 
     public static void main(String[] args) {
@@ -12,5 +14,7 @@ public class Test {
 
 // after we initialize them in static block , it gives the given values
 
+        System.out.println(Arrays.toString(Data.arr));
+        System.out.println(Data.list);
     }
 }
