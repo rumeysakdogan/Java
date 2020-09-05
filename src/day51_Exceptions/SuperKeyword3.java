f6e2d156cfea592a1339faaa3b98c9da40d623d3
rumeysakdogan <dgn.rumeysaa@gmail.com>
2020-09-05 10:57:43 -0600

    add SuperKeyword3

---
+++ src/day51_Exceptions/SuperKeyword3.java
@@ -0,0 +1,23 @@
+package day51_Exceptions;
+
+class C{
+
+    public C(int a){
+        System.out.println("Super class' int arg constructor");
+    }
+}
+
+
+public class SuperKeyword3 extends C{   // Sub class has to call a constructor from super class
+
+
+    public SuperKeyword3(int a) {
+        super(100);
+        System.out.println("Sub class' constructor with int arg");
+    }
+
+    public static void main(String[] args) {
+        new SuperKeyword3(200);
+
+    }
+}
